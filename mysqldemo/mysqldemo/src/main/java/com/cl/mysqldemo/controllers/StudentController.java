d8064af3fcd9896ff0959a9e97c16f8869449435
Almonther Alwaili <montherwaili@gmail.com>
2026-09-02 18:06:42 +0400

    make it final

--- mysqldemo/mysqldemo/src/main/java/com/cl/mysqldemo/controllers/StudentController.java
+++ mysqldemo/mysqldemo/src/main/java/com/cl/mysqldemo/controllers/StudentController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RequestMapping("student")
 public class StudentController {
 
-    StudentService studentService;
+    private final StudentService studentService;
 
     @Autowired
     public StudentController(StudentService studentService) {
