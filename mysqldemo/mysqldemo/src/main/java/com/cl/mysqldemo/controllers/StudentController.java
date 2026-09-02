c953c0f3aa8e6e1afbe02b65383c1349d4bd473e
Almonther Alwaili <montherwaili@gmail.com>
2026-09-02 18:04:35 +0400

    import required libraries

--- mysqldemo/mysqldemo/src/main/java/com/cl/mysqldemo/controllers/StudentController.java
+++ mysqldemo/mysqldemo/src/main/java/com/cl/mysqldemo/controllers/StudentController.java
@@ -1,11 +1,12 @@
 package com.cl.mysqldemo.controllers;
 
+import com.cl.mysqldemo.dto.StudentDTO;
+import com.cl.mysqldemo.entities.Student;
 import com.cl.mysqldemo.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("student")
