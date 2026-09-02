001c1d0020fa3b78220b47ed2376652c5a9b7fdc
Almonther Alwaili <montherwaili@gmail.com>
2026-09-02 17:27:48 +0400

    fix auto generated id

--- mysqldemo/mysqldemo/src/main/java/com/cl/mysqldemo/entities/School.java
+++ mysqldemo/mysqldemo/src/main/java/com/cl/mysqldemo/entities/School.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -11,14 +12,16 @@ import java.util.List;
 @Setter
 @Entity
 public class School {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String location;
 
     @OneToMany(cascade = CascadeType.ALL)
-    private List<Student> students;
+    private List<Student> students = new ArrayList<>();
 
     private Boolean isActive;
     private Date createdDate;
