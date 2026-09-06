65b769d5eea9dfe140b32ce418a9d8e31483c1d4
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 10:52:21 +0400

    Fill in Class Book

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Book.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Book.java
@@ -1,4 +1,41 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Book {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Book extends BaseClass {
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String isbn;
+
+    @Column(name = "total_copies", nullable = false)
+    private Integer totalCopies;
+
+    @Column(name = "available_copies", nullable = false)
+    private Integer availableCopies;
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Loan> loans = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Reservation> reservations = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Review> reviews = new ArrayList<>();
 }
