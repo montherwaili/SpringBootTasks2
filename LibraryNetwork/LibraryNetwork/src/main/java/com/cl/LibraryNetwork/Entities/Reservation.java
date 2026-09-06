e18bf75b539e32375a6ad9f1c380601ed2447d35
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 11:16:49 +0400

    Fill in Reservation class

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Reservation.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Reservation.java
@@ -1,4 +1,26 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Reservation {
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reservations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reservation extends BaseClass {
+
+    @Column(name = "reservation_date", nullable = false)
+    private LocalDateTime reservationDate;
+
+    @Column(nullable = false, length = 30)
+    private String status;
+
 }
