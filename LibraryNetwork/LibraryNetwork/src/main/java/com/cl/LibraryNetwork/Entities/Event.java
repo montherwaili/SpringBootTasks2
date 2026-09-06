aa51f003c21bf6c487e478a4cdcce349e774b9d5
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 11:27:46 +0400

    Fill in class Event

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Event.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Event.java
@@ -1,4 +1,27 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Event {
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Event extends BaseClass {
+
+    @Column(nullable = false, length = 150)
+    private String title;
+
+    @Column(name = "event_date", nullable = false)
+    private LocalDateTime eventDate;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
 }
