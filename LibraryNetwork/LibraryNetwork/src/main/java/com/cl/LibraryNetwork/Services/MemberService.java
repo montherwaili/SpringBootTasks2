7afd20dba5b78f3007576d906b7fbde4557b5aca
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:39:34 +0400

    fill in MemberService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/MemberService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/MemberService.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.Services;
 
+import com.cl.LibraryNetwork.Entities.Member;
+import com.cl.LibraryNetwork.Repositories.MemberRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class MemberService {
+    private final MemberRepository repository;
+
+    public MemberService(MemberRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Member> getAll() { return repository.findAll(); }
+    public Member getById(Long id) { return repository.findById(id).orElse(null); }
+    public Member save(Member entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
