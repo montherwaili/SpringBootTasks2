1cd294b7cfe09068fdd40a2bb44aed971cd77149
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 12:23:23 +0400

    add ReservationRepository

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/ReservationRepository.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/ReservationRepository.java
@@ -1,4 +1,8 @@
 package com.cl.LibraryNetwork.Repositories;
 
-public interface ReservationRepository {
-}
+import com.cl.LibraryNetwork.Entities.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {}
