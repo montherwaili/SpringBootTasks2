e35cc7ebb4e2723dc80120cdc4aab0f931f3ba9d
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 12:21:17 +0400

    add AuthorRepository

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/AuthorRepository.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/AuthorRepository.java
@@ -1,4 +1,8 @@
 package com.cl.LibraryNetwork.Repositories;
 
-public interface AuthorRepository {
-}
+import com.cl.LibraryNetwork.Entities.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {}
