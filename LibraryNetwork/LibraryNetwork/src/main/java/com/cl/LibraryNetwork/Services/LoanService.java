78b97e6f334ff0082f595fc74fbea8f14c02a828
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:39:10 +0400

    fill in LoanService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/LoanService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/LoanService.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.Services;
 
+import com.cl.LibraryNetwork.Entities.Loan;
+import com.cl.LibraryNetwork.Repositories.LoanRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class LoanService {
+    private final LoanRepository repository;
+
+    public LoanService(LoanRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Loan> getAll() { return repository.findAll(); }
+    public Loan getById(Long id) { return repository.findById(id).orElse(null); }
+    public Loan save(Loan entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
