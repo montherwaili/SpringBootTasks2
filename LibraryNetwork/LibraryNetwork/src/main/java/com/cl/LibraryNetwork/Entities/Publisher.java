09526aa1d033b4138e7d0f61ec524450d9da42cd
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 10:48:30 +0400

    Fill in class Publisher

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Publisher.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Publisher.java
@@ -1,4 +1,30 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Publisher {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Publisher extends BaseClass {
+
+    @Column(nullable = false, length = 120)
+    private String name;
+
+    @Column(length = 255)
+    private String address;
+
+    @Column(name = "contact_email", length = 100)
+    private String contactEmail;
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Book> books = new ArrayList<>();
 }
