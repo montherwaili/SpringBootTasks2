751760801680c4d80f7eebad633036a6aa41b8ff
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 11:04:14 +0400

    Fill in class Member

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Member.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Member.java
@@ -1,4 +1,49 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Member {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "members")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
+    @Column(name = "membership_type", nullable = false, length = 30)
+    private String membershipType;
+
+    @JoinColumn(name = "branch_id", nullable = false)
+    private Branch branch;
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Loan> loans = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Reservation> reservations = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Fine> fines = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Review> reviews = new ArrayList<>();
 }
