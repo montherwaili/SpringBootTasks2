5a87457e0bcb479c02eaba79a369fa64c47a865f
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:15:13 +0400

    fill in BranchController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/BranchController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/BranchController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Branch;
+import com.cl.LibraryNetwork.Services.BranchService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/branches")
 public class BranchController {
+    private final BranchService service;
+
+    public BranchController(BranchService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Branch> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Branch getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Branch create(@RequestBody Branch entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
