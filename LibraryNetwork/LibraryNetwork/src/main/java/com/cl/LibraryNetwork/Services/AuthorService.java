1cb58d91c8d0918f0c1bab9ff1491b471e003496
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:13:00 +0400

    Update AuthorService.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/AuthorService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/AuthorService.java
@@ -16,5 +16,14 @@ public class AuthorService {
     public List<Author> getAll() { return repository.findAll(); }
     public Author getById(Long id) { return repository.findById(id).orElse(null); }
     public Author save(Author entity) { return repository.save(entity); }
+
+    public Author update(Long id, Author entity) {
+        if (repository.existsById(id)) {
+            entity.setId(id);
+            return repository.save(entity);
+        }
+        return null;
+    }
+
     public void delete(Long id) { repository.deleteById(id); }
 }
