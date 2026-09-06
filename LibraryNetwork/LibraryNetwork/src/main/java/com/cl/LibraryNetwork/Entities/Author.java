2c99dbdca0e23317c37219b5b5994409d9f90cd8
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 10:42:51 +0400

    Fill in class Author

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Author.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Author.java
@@ -1,4 +1,32 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Author {
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Author extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(length = 60)
+    private String nationality;
+
+    @Column(columnDefinition = "TEXT")
+    private String biography;
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Book> books = new ArrayList<>();
 }
