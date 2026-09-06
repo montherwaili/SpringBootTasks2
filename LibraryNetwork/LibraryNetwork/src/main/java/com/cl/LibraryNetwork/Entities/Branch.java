791bbae26948636152a42ef38876c81f6d1f726d
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 10:41:53 +0400

    add relationship

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Branch.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Branch.java
@@ -5,16 +5,21 @@ package com.cl.LibraryNetwork.Entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "branches")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@SQLDelete(sql = "UPDATE branches SET is_active = false WHERE id = ?")
+@SQLRestriction("is_active = true")
 public class Branch extends BaseClass {
 
     @Column(nullable = false, length = 100)
@@ -23,18 +28,22 @@ public class Branch extends BaseClass {
     @Column(nullable = false, length = 255)
     private String location;
 
+    @OneToMany(mappedBy = "branch", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Book> books = new ArrayList<>();
 
+    @OneToMany(mappedBy = "branch", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Member> members = new ArrayList<>();
 
+    @OneToMany(mappedBy = "branch", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Staff> staffMembers = new ArrayList<>();
 
+    @OneToMany(mappedBy = "branch", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Event> events = new ArrayList<>();
