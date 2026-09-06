60dd2a4949e28a47311eeed6ef119f31d0bb69b4
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:40:05 +0400

    fill in ReservationService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/ReservationService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/ReservationService.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.Services;
 
+import com.cl.LibraryNetwork.Entities.Reservation;
+import com.cl.LibraryNetwork.Repositories.ReservationRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class ReservationService {
+    private final ReservationRepository repository;
+
+    public ReservationService(ReservationRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Reservation> getAll() { return repository.findAll(); }
+    public Reservation getById(Long id) { return repository.findById(id).orElse(null); }
+    public Reservation save(Reservation entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
