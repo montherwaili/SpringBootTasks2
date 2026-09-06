7a47d9a47c213f06aa7d77ae3951b9efbba03f34
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:18:03 +0400

    fill in  MemberController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/MemberController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/MemberController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Member;
+import com.cl.LibraryNetwork.Services.MemberService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
 public class MemberController {
+    private final MemberService service;
+
+    public MemberController(MemberService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Member> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Member getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Member create(@RequestBody Member entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
