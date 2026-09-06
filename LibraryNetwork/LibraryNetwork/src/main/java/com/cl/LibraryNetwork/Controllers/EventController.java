7e4eb899cb22276973429123990e21fb3307441b
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:16:01 +0400

    fill in EventController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/EventController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/EventController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Event;
+import com.cl.LibraryNetwork.Services.EventService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
 public class EventController {
+    private final EventService service;
+
+    public EventController(EventService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Event> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Event getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Event create(@RequestBody Event entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
