29fa20c66041cd577eb73973fb60a9ed522e0126
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:16:22 +0400

    fill in FineController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/FineController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/FineController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Fine;
+import com.cl.LibraryNetwork.Services.FineService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/fines")
 public class FineController {
+    private final FineService service;
+
+    public FineController(FineService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Fine> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Fine getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Fine create(@RequestBody Fine entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
