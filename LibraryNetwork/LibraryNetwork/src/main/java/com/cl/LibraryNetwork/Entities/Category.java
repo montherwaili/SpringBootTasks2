7307bbd23a26306a4c6842d8c7a4ae994b3b3475
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 10:45:40 +0400

    add the relationships

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Category.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Category.java
@@ -4,16 +4,21 @@ package com.cl.LibraryNetwork.Entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "categories")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@SQLDelete(sql = "UPDATE categories SET is_active = false WHERE id = ?")
+@SQLRestriction("is_active = true")
 public class Category extends BaseClass {
 
     @Column(nullable = false, unique = true, length = 60)
@@ -22,6 +27,7 @@ public class Category extends BaseClass {
     @Column(length = 255)
     private String description;
 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Book> books = new ArrayList<>();
