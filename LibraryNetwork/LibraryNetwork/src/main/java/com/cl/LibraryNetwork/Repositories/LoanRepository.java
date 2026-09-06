6d73874e8cbd1c26062fb9acb18deb548df0f018
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 12:22:44 +0400

    add LoanRepository

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/LoanRepository.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/LoanRepository.java
@@ -1,4 +1,8 @@
 package com.cl.LibraryNetwork.Repositories;
 
-public interface LoanRepository {
-}
+import com.cl.LibraryNetwork.Entities.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {}
