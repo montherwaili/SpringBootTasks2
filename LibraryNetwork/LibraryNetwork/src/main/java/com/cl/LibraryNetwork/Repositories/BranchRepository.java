f6e3976b489e5265c40cf134f8345851ccb2bd28
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 12:21:47 +0400

    add BranchRepository

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/BranchRepository.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/BranchRepository.java
@@ -1,4 +1,8 @@
 package com.cl.LibraryNetwork.Repositories;
 
-public interface BranchRepository {
-}
+import com.cl.LibraryNetwork.Entities.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BranchRepository extends JpaRepository<Branch, Long> {}
