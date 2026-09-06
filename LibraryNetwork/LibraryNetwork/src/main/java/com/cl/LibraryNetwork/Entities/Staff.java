225a58aefcaa0f8e7ade4b19ec759851c7a42c6d
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 11:25:32 +0400

    Fill in Staff class

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Staff.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Staff.java
@@ -1,4 +1,24 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Staff {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "staff")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Staff extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String role;
+
+    @Column(name = "phone_number", length = 20)
+    private String phoneNumber;
+
 }
