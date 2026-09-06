96d2bc893c0a53f3830de10bfb4e689db0ef550f
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:18:38 +0400

    fill in ReservationController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/ReservationController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/ReservationController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Reservation;
+import com.cl.LibraryNetwork.Services.ReservationService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservations")
 public class ReservationController {
+    private final ReservationService service;
+
+    public ReservationController(ReservationService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Reservation> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Reservation getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Reservation create(@RequestBody Reservation entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
