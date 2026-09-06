5f9dc6917a719f8022350cc2b8ba7842f0b49ed4
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:18:56 +0400

    fill in ReviewController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/ReviewController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/ReviewController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Review;
+import com.cl.LibraryNetwork.Services.ReviewService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reviews")
 public class ReviewController {
+    private final ReviewService service;
+
+    public ReviewController(ReviewService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Review> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Review getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Review create(@RequestBody Review entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
