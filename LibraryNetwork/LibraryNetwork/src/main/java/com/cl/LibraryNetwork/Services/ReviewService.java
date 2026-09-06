cddaba0ec6238c58b47e09e6f5b2f2e414b2f73c
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:40:21 +0400

    fill in ReviewService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/ReviewService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/ReviewService.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.Services;
 
+import com.cl.LibraryNetwork.Entities.Review;
+import com.cl.LibraryNetwork.Repositories.ReviewRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class ReviewService {
+    private final ReviewRepository repository;
+
+    public ReviewService(ReviewRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Review> getAll() { return repository.findAll(); }
+    public Review getById(Long id) { return repository.findById(id).orElse(null); }
+    public Review save(Review entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
