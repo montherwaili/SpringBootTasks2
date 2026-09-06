bd5f9a2f505f54efa67ca2e5e2370736b87140cd
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:14:40 +0400

    fill in BookController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/BookController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/BookController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Book;
+import com.cl.LibraryNetwork.Services.BookService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+    private final BookService service;
+
+    public BookController(BookService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Book> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Book getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Book create(@RequestBody Book entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
