be92bf3b18c9d8b673dffaaaa488110a3e286933
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 12:23:10 +0400

    add PublisherRepository

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/PublisherRepository.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/PublisherRepository.java
@@ -1,4 +1,8 @@
 package com.cl.LibraryNetwork.Repositories;
 
-public interface PublisherRepository {
-}
+import com.cl.LibraryNetwork.Entities.Publisher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<Publisher, Long> {}
