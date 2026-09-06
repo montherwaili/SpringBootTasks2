c6f5829b1cb2c1f790709656ddb4c344bbfbe735
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:32:39 +0400

    Fill in BranchService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/BranchService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/BranchService.java
@@ -1,4 +1,21 @@
 package com.cl.LibraryNetwork.Services;
 
+
+import com.cl.LibraryNetwork.Entities.Branch;
+import com.cl.LibraryNetwork.Repositories.BranchRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class BranchService {
+    private final BranchRepository repository;
+
+    public BranchService(BranchRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Branch> getAll() { return repository.findAll(); }
+    public Branch getById(Long id) { return repository.findById(id).orElse(null); }
+    public Branch save(Branch entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
