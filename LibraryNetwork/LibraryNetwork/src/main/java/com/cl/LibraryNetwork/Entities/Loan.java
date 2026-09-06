da1aa242d9d406fa585713942b07a873c82d87c1
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 11:14:38 +0400

    Fill in Loan class

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Loan.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Loan.java
@@ -1,4 +1,36 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Loan {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "loans")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Loan extends BaseClass {
+
+    @Column(name = "loan_date", nullable = false)
+    private LocalDate loanDate;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDate dueDate;
+
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    @Column(name = "is_returned", nullable = false)
+    @Builder.Default
+    private Boolean isReturned = false;
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Fine> fines = new ArrayList<>();
 }
