6061bf09216e755d7341271060e0b21186c0113f
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 11:25:52 +0400

    make the relationships

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Staff.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Staff.java
@@ -2,6 +2,8 @@ package com.cl.LibraryNetwork.Entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "staff")
@@ -10,6 +12,8 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@SQLDelete(sql = "UPDATE staff SET is_active = false WHERE id = ?")
+@SQLRestriction("is_active = true")
 public class Staff extends BaseClass {
 
     @Column(nullable = false, length = 100)
@@ -21,4 +25,7 @@ public class Staff extends BaseClass {
     @Column(name = "phone_number", length = 20)
     private String phoneNumber;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id", nullable = false)
+    private Branch branch;
 }
