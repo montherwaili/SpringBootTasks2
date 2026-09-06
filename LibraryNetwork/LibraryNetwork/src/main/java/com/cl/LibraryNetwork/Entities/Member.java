77df1637b39f0371b59bdb07f15d7e68e0647bb8
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 11:04:44 +0400

    make the required relationship

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Member.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Member.java
@@ -3,6 +3,8 @@ package com.cl.LibraryNetwork.Entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +16,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@SQLDelete(sql = "UPDATE members SET is_active = false WHERE id = ?")
+@SQLRestriction("is_active = true")
 public class Member extends BaseClass {
 
     @Column(nullable = false, length = 100)
@@ -28,21 +32,26 @@ public class Member extends BaseClass {
     @Column(name = "membership_type", nullable = false, length = 30)
     private String membershipType;
 
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "branch_id", nullable = false)
     private Branch branch;
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Loan> loans = new ArrayList<>();
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Reservation> reservations = new ArrayList<>();
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Fine> fines = new ArrayList<>();
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Review> reviews = new ArrayList<>();
