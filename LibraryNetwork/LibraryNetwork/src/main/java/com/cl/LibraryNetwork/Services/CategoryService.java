f5d7ee6ef94f34cd4e4a39a63b1b67de7f6e1d76
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:13:42 +0400

    Update CategoryService.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/CategoryService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/CategoryService.java
@@ -16,5 +16,14 @@ public class CategoryService {
     public List<Category> getAll() { return repository.findAll(); }
     public Category getById(Long id) { return repository.findById(id).orElse(null); }
     public Category save(Category entity) { return repository.save(entity); }
+
+    public Category update(Long id, Category entity) {
+        if (repository.existsById(id)) {
+            entity.setId(id);
+            return repository.save(entity);
+        }
+        return null;
+    }
+
     public void delete(Long id) { repository.deleteById(id); }
 }
