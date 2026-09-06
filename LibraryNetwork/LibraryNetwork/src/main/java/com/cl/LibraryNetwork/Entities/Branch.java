03e5046bb90495bedf137fdade35f407a973ab17
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 10:41:27 +0400

    Fill in Branch

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Branch.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Branch.java
@@ -1,4 +1,41 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Branch {
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Branch extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 255)
+    private String location;
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Book> books = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Member> members = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Staff> staffMembers = new ArrayList<>();
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Event> events = new ArrayList<>();
 }
