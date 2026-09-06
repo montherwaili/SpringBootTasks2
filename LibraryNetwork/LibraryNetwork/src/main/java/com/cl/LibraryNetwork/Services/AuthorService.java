9930d72cbf3d6116801d6f84b9c179c682e356e7
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:33:30 +0400

    Fill AuthorService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/AuthorService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/AuthorService.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.Services;
 
+import com.cl.LibraryNetwork.Entities.Author;
+import com.cl.LibraryNetwork.Repositories.AuthorRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class AuthorService {
+    private final AuthorRepository repository;
+
+    public AuthorService(AuthorRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Author> getAll() { return repository.findAll(); }
+    public Author getById(Long id) { return repository.findById(id).orElse(null); }
+    public Author save(Author entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
