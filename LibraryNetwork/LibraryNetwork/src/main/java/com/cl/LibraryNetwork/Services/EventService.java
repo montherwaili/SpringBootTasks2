31d1b5b2e36cd3e44696b4dd97c7a0e813b48aad
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:38:23 +0400

    fill in EventService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/EventService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/EventService.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.Services;
 
+import com.cl.LibraryNetwork.Entities.Event;
+import com.cl.LibraryNetwork.Repositories.EventRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class EventService {
+    private final EventRepository repository;
+
+    public EventService(EventRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Event> getAll() { return repository.findAll(); }
+    public Event getById(Long id) { return repository.findById(id).orElse(null); }
+    public Event save(Event entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
