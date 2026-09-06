da0c002f4a8e69f94fe79c6c2ffe71a3d24d99d6
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:18:21 +0400

    fill in PublisherController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/PublisherController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/PublisherController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Publisher;
+import com.cl.LibraryNetwork.Services.PublisherService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/publishers")
 public class PublisherController {
+    private final PublisherService service;
+
+    public PublisherController(PublisherService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Publisher> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Publisher getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Publisher create(@RequestBody Publisher entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
