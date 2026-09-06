6ec6dd6a4a310ac9df9b6efa3edd682c5cc6948e
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:38:57 +0400

    fill in FineService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/FineService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/FineService.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.Services;
 
+import com.cl.LibraryNetwork.Entities.Fine;
+import com.cl.LibraryNetwork.Repositories.FineRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class FineService {
+    private final FineRepository repository;
+
+    public FineService(FineRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Fine> getAll() { return repository.findAll(); }
+    public Fine getById(Long id) { return repository.findById(id).orElse(null); }
+    public Fine save(Fine entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
