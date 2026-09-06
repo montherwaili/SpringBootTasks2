1c206b4617047a48be9d0fb555acbb81079a1849
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 12:22:30 +0400

    add FineRepository

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/FineRepository.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/FineRepository.java
@@ -1,4 +1,8 @@
 package com.cl.LibraryNetwork.Repositories;
 
-public interface FineRepository {
-}
+import com.cl.LibraryNetwork.Entities.Fine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FineRepository extends JpaRepository<Fine, Long> {}
