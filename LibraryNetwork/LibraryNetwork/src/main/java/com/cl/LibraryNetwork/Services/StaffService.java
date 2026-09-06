a253dda890794f71621764f273e346243fbfe734
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:40:39 +0400

    fill in StaffService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/StaffService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/StaffService.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.Services;
 
+import com.cl.LibraryNetwork.Entities.Staff;
+import com.cl.LibraryNetwork.Repositories.StaffRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class StaffService {
+    private final StaffRepository repository;
+
+    public StaffService(StaffRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Staff> getAll() { return repository.findAll(); }
+    public Staff getById(Long id) { return repository.findById(id).orElse(null); }
+    public Staff save(Staff entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
