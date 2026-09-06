a6731dd2ecf9c04fa048a3f51892fd7fd8c4600a
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 12:22:13 +0400

    add EventRepository

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/EventRepository.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/EventRepository.java
@@ -1,4 +1,8 @@
 package com.cl.LibraryNetwork.Repositories;
 
-public interface EventRepository {
-}
+import com.cl.LibraryNetwork.Entities.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {}
