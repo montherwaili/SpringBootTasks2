e0fc8ffb98c2e3fd6b6e70211d9768117a4553c6
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:14:20 +0400

    fill in AuthorController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/AuthorController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/AuthorController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Author;
+import com.cl.LibraryNetwork.Services.AuthorService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/authors")
 public class AuthorController {
+    private final AuthorService service;
+
+    public AuthorController(AuthorService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Author> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Author getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Author create(@RequestBody Author entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
