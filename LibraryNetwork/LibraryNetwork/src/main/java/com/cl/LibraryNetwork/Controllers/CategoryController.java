c88fddbad8956c4a2ea76e9a6c20b1a122aa138d
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 15:15:37 +0400

    fill in CategoryController

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/CategoryController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/CategoryController.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Controllers;
 
+import com.cl.LibraryNetwork.Entities.Category;
+import com.cl.LibraryNetwork.Services.CategoryService;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Category> getAll() { return service.getAll(); }
+
+    @GetMapping("/{id}")
+    public Category getById(@PathVariable Long id) { return service.getById(id); }
+
+    @PostMapping
+    public Category create(@RequestBody Category entity) { return service.save(entity); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) { service.delete(id); }
 }
