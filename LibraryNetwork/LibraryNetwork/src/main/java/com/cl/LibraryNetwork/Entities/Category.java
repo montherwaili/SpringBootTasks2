220891d4989c1b5ce87faf1721142c34d9e64b69
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 10:45:25 +0400

    Fill in class Category

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Category.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Category.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Category {
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category extends BaseClass {
+
+    @Column(nullable = false, unique = true, length = 60)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    @JsonIgnore
+    @Builder.Default
+    private List<Book> books = new ArrayList<>();
 }
