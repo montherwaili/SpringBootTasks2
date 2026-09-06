c0b52f9e190161942f15302628ad1b62447b5111
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 10:48:47 +0400

    make the relationships

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Publisher.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Publisher.java
@@ -3,16 +3,21 @@ package com.cl.LibraryNetwork.Entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "publishers")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@SQLDelete(sql = "UPDATE publishers SET is_active = false WHERE id = ?")
+@SQLRestriction("is_active = true")
 public class Publisher extends BaseClass {
 
     @Column(nullable = false, length = 120)
@@ -24,6 +29,7 @@ public class Publisher extends BaseClass {
     @Column(name = "contact_email", length = 100)
     private String contactEmail;
 
+    @OneToMany(mappedBy = "publisher", cascade = CascadeType.ALL)
     @JsonIgnore
     @Builder.Default
     private List<Book> books = new ArrayList<>();
