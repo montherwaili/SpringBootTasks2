e8d2941f9e4f3805a08b48a643aa46ab11bc1ef5
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 12:23:55 +0400

    add StaffRepository

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/StaffRepository.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Repositories/StaffRepository.java
@@ -1,4 +1,8 @@
 package com.cl.LibraryNetwork.Repositories;
 
-public interface StaffRepository {
-}
+import com.cl.LibraryNetwork.Entities.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffRepository extends JpaRepository<Staff, Long> {}
