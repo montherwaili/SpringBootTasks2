bb1a693dbd51873d49b90b974887b5c73b232933
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 14:39:49 +0400

    fill in PublisherService

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/PublisherService.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Services/PublisherService.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.Services;
 
+import com.cl.LibraryNetwork.Entities.Publisher;
+import com.cl.LibraryNetwork.Repositories.PublisherRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class PublisherService {
+    private final PublisherRepository repository;
+
+    public PublisherService(PublisherRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Publisher> getAll() { return repository.findAll(); }
+    public Publisher getById(Long id) { return repository.findById(id).orElse(null); }
+    public Publisher save(Publisher entity) { return repository.save(entity); }
+    public void delete(Long id) { repository.deleteById(id); }
 }
