c06d791c41b9be529a52a12d23cc67444dc98d0a
Almonther Alwaili <montherwaili@gmail.com>
2026-09-06 11:22:48 +0400

    Fill in class review

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Review.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Entities/Review.java
@@ -1,4 +1,28 @@
 package com.cl.LibraryNetwork.Entities;
 
-public class Review {
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reviews")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Review extends BaseClass {
+
+    @Column(nullable = false)
+    private Integer rating;
+
+    @Column(columnDefinition = "TEXT")
+    private String comment;
+
+    @Column(name = "review_date", nullable = false)
+    private LocalDateTime reviewDate;
+
 }
