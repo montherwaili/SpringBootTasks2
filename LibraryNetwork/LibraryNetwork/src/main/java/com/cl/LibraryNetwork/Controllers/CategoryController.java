c1b44ceb24b89f412730f08a169114a329c3b1cb
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 14:36:29 +0400

    Update CategoryController.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/CategoryController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/CategoryController.java
@@ -1,33 +1,46 @@
 package com.cl.LibraryNetwork.Controllers;
 
+
 import com.cl.LibraryNetwork.Entities.Category;
 import com.cl.LibraryNetwork.Services.CategoryService;
+import com.cl.LibraryNetwork.dto.CategoryDTO;
+import com.cl.LibraryNetwork.mapper.CategoryMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+@RequiredArgsConstructor
 public class CategoryController {
-    private final CategoryService service;
 
-    public CategoryController(CategoryService service) {
-        this.service = service;
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
+
+    @PostMapping
+    public CategoryDTO createCategory(@RequestBody Category category) {
+        return categoryMapper.convertToDTO(categoryService.save(category));
     }
 
     @GetMapping
-    public List<Category> getAll() { return service.getAll(); }
+    public List<CategoryDTO> getAllCategories() {
+        return categoryMapper.convertToDTO(categoryService.findAll());
+    }
 
     @GetMapping("/{id}")
-    public Category getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Category create(@RequestBody Category entity) { return service.save(entity); }
+    public CategoryDTO getCategoryById(@PathVariable Long id) {
+        return categoryMapper.convertToDTO(categoryService.findById(id));
+    }
 
     @PutMapping("/{id}")
-    public Category update(@PathVariable Long id, @RequestBody Category entity) {
-        return service.update(id, entity);
+    public CategoryDTO updateCategory(@PathVariable Long id, @RequestBody Category category) {
+        category.setId(id);
+        return categoryMapper.convertToDTO(categoryService.save(category));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void deleteCategory(@PathVariable Long id) {
+        categoryService.deleteById(id);
+    }
 }
