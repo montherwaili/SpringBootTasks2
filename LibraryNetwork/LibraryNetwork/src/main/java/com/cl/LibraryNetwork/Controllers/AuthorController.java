e614311df94462502510db437cf2530b77bd6c23
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 14:36:15 +0400

    Update AuthorController.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/AuthorController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/AuthorController.java
@@ -1,33 +1,46 @@
 package com.cl.LibraryNetwork.Controllers;
 
+
 import com.cl.LibraryNetwork.Entities.Author;
 import com.cl.LibraryNetwork.Services.AuthorService;
+import com.cl.LibraryNetwork.dto.AuthorDTO;
+import com.cl.LibraryNetwork.mapper.AuthorMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/authors")
+@RequiredArgsConstructor
 public class AuthorController {
-    private final AuthorService service;
 
-    public AuthorController(AuthorService service) {
-        this.service = service;
+    private final AuthorService authorService;
+    private final AuthorMapper authorMapper;
+
+    @PostMapping
+    public AuthorDTO createAuthor(@RequestBody Author author) {
+        return authorMapper.convertToDTO(authorService.save(author));
     }
 
     @GetMapping
-    public List<Author> getAll() { return service.getAll(); }
+    public List<AuthorDTO> getAllAuthors() {
+        return authorMapper.convertToDTO(authorService.findAll());
+    }
 
     @GetMapping("/{id}")
-    public Author getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Author create(@RequestBody Author entity) { return service.save(entity); }
+    public AuthorDTO getAuthorById(@PathVariable Long id) {
+        return authorMapper.convertToDTO(authorService.findById(id));
+    }
 
     @PutMapping("/{id}")
-    public Author update(@PathVariable Long id, @RequestBody Author entity) {
-        return service.update(id, entity);
+    public AuthorDTO updateAuthor(@PathVariable Long id, @RequestBody Author author) {
+        author.setId(id);
+        return authorMapper.convertToDTO(authorService.save(author));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void deleteAuthor(@PathVariable Long id) {
+        authorService.deleteById(id);
+    }
 }
