ce31139ded96d033dc5c7f6b44fc1afcf82ec4bc
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 10:49:48 +0400

    Update BookDTO.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/BookDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/BookDTO.java
@@ -1,4 +1,20 @@
 package com.cl.LibraryNetwork.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDTO {
+    private Long id;
+    private String title;
+    private String isbn;
+    private Long authorId;
+    private Long categoryId;
+    private Long publisherId;
 }
