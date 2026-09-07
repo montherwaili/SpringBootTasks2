29079a04f17682da4dc7723c7033c2532ed443c5
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 11:59:40 +0400

    Update BranchMapper.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/mapper/BranchMapper.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/mapper/BranchMapper.java
@@ -1,4 +1,27 @@
 package com.cl.LibraryNetwork.mapper;
 
+import com.cl.LibraryNetwork.dto.BranchDTO;
+import com.cl.LibraryNetwork.Entities.Branch;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class BranchMapper {
+
+    public BranchDTO convertToDTO(Branch entity) {
+        if (entity == null) return null;
+        return BranchDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .location(entity.getLocation())
+                .build();
+    }
+
+    public List<BranchDTO> convertToDTO(List<Branch> entities) {
+        return entities.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 }
