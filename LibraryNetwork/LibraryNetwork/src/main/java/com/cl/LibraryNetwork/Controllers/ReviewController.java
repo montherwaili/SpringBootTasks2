dc4ed475ccbfbb8ae6e8509fa7a183774e577d84
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 14:36:52 +0400

    Update ReviewController.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/ReviewController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/ReviewController.java
@@ -1,33 +1,46 @@
 package com.cl.LibraryNetwork.Controllers;
 
+
 import com.cl.LibraryNetwork.Entities.Review;
 import com.cl.LibraryNetwork.Services.ReviewService;
+import com.cl.LibraryNetwork.dto.ReviewDTO;
+import com.cl.LibraryNetwork.mapper.ReviewMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/reviews")
+@RequiredArgsConstructor
 public class ReviewController {
-    private final ReviewService service;
 
-    public ReviewController(ReviewService service) {
-        this.service = service;
+    private final ReviewService reviewService;
+    private final ReviewMapper reviewMapper;
+
+    @PostMapping
+    public ReviewDTO createReview(@RequestBody Review review) {
+        return reviewMapper.convertToDTO(reviewService.save(review));
     }
 
     @GetMapping
-    public List<Review> getAll() { return service.getAll(); }
+    public List<ReviewDTO> getAllReviews() {
+        return reviewMapper.convertToDTO(reviewService.findAll());
+    }
 
     @GetMapping("/{id}")
-    public Review getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Review create(@RequestBody Review entity) { return service.save(entity); }
+    public ReviewDTO getReviewById(@PathVariable Long id) {
+        return reviewMapper.convertToDTO(reviewService.findById(id));
+    }
 
     @PutMapping("/{id}")
-    public Review update(@PathVariable Long id, @RequestBody Review entity) {
-        return service.update(id, entity);
+    public ReviewDTO updateReview(@PathVariable Long id, @RequestBody Review review) {
+        review.setId(id);
+        return reviewMapper.convertToDTO(reviewService.save(review));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void deleteReview(@PathVariable Long id) {
+        reviewService.deleteById(id);
+    }
 }
