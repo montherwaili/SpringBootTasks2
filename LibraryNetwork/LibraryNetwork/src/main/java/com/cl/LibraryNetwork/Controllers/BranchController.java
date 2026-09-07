9447d4d0acdd930af5749cfa657c4fabc2e7ff9f
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 14:36:27 +0400

    Update BranchController.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/BranchController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/BranchController.java
@@ -2,32 +2,44 @@ package com.cl.LibraryNetwork.Controllers;
 
 import com.cl.LibraryNetwork.Entities.Branch;
 import com.cl.LibraryNetwork.Services.BranchService;
+import com.cl.LibraryNetwork.dto.BranchDTO;
+import com.cl.LibraryNetwork.mapper.BranchMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/branches")
+@RequiredArgsConstructor
 public class BranchController {
-    private final BranchService service;
 
-    public BranchController(BranchService service) {
-        this.service = service;
+    private final BranchService branchService;
+    private final BranchMapper branchMapper;
+
+    @PostMapping
+    public BranchDTO createBranch(@RequestBody Branch branch) {
+        return branchMapper.convertToDTO(branchService.save(branch));
     }
 
     @GetMapping
-    public List<Branch> getAll() { return service.getAll(); }
+    public List<BranchDTO> getAllBranches() {
+        return branchMapper.convertToDTO(branchService.findAll());
+    }
 
     @GetMapping("/{id}")
-    public Branch getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Branch create(@RequestBody Branch entity) { return service.save(entity); }
+    public BranchDTO getBranchById(@PathVariable Long id) {
+        return branchMapper.convertToDTO(branchService.findById(id));
+    }
 
     @PutMapping("/{id}")
-    public Branch update(@PathVariable Long id, @RequestBody Branch entity) {
-        return service.update(id, entity);
+    public BranchDTO updateBranch(@PathVariable Long id, @RequestBody Branch branch) {
+        branch.setId(id);
+        return branchMapper.convertToDTO(branchService.save(branch));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void deleteBranch(@PathVariable Long id) {
+        branchService.deleteById(id);
+    }
 }
