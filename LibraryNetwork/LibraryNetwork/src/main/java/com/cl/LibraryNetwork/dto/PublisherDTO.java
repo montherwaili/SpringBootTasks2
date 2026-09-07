0c7b19ee28b9b707e1b28db2cad131d5dfce7fc9
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 10:50:03 +0400

    Update PublisherDTO.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/PublisherDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/PublisherDTO.java
@@ -1,4 +1,15 @@
 package com.cl.LibraryNetwork.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PublisherDTO {
+    private Long id;
+    private String name;
 }
