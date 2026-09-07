f36c456886a3d6a5e7cbadc9130e067442093b35
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 10:49:41 +0400

    Update AuthorDTO.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/AuthorDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/AuthorDTO.java
@@ -1,4 +1,17 @@
 package com.cl.LibraryNetwork.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthorDTO {
+    private Long id;
+    private String name;
+    private String bio;
 }
