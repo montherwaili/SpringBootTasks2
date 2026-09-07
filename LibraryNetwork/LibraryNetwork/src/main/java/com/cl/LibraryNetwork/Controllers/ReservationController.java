ae6bfef7fbad09344c11c1b46e5d9e7eb848acc3
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 14:36:49 +0400

    Update ReservationController.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/ReservationController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/ReservationController.java
@@ -1,33 +1,46 @@
 package com.cl.LibraryNetwork.Controllers;
 
+
 import com.cl.LibraryNetwork.Entities.Reservation;
 import com.cl.LibraryNetwork.Services.ReservationService;
+import com.cl.LibraryNetwork.dto.ReservationDTO;
+import com.cl.LibraryNetwork.mapper.ReservationMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/reservations")
+@RequiredArgsConstructor
 public class ReservationController {
-    private final ReservationService service;
 
-    public ReservationController(ReservationService service) {
-        this.service = service;
+    private final ReservationService reservationService;
+    private final ReservationMapper reservationMapper;
+
+    @PostMapping
+    public ReservationDTO createReservation(@RequestBody Reservation reservation) {
+        return reservationMapper.convertToDTO(reservationService.save(reservation));
     }
 
     @GetMapping
-    public List<Reservation> getAll() { return service.getAll(); }
+    public List<ReservationDTO> getAllReservations() {
+        return reservationMapper.convertToDTO(reservationService.findAll());
+    }
 
     @GetMapping("/{id}")
-    public Reservation getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Reservation create(@RequestBody Reservation entity) { return service.save(entity); }
+    public ReservationDTO getReservationById(@PathVariable Long id) {
+        return reservationMapper.convertToDTO(reservationService.findById(id));
+    }
 
     @PutMapping("/{id}")
-    public Reservation update(@PathVariable Long id, @RequestBody Reservation entity) {
-        return service.update(id, entity);
+    public ReservationDTO updateReservation(@PathVariable Long id, @RequestBody Reservation reservation) {
+        reservation.setId(id);
+        return reservationMapper.convertToDTO(reservationService.save(reservation));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void deleteReservation(@PathVariable Long id) {
+        reservationService.deleteById(id);
+    }
 }
