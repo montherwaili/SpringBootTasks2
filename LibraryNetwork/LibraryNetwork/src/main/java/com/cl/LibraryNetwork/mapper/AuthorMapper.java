5d429df571f5fe662fe93dce1a1c73df0fcb43da
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 11:59:34 +0400

    Update AuthorMapper.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/mapper/AuthorMapper.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/mapper/AuthorMapper.java
@@ -1,4 +1,27 @@
 package com.cl.LibraryNetwork.mapper;
 
+import com.cl.LibraryNetwork.dto.AuthorDTO;
+import com.cl.LibraryNetwork.Entities.Author;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class AuthorMapper {
+
+    public AuthorDTO convertToDTO(Author entity) {
+        if (entity == null) return null;
+        return AuthorDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .bio(entity.getBiography())
+                .build();
+    }
+
+    public List<AuthorDTO> convertToDTO(List<Author> entities) {
+        return entities.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 }
