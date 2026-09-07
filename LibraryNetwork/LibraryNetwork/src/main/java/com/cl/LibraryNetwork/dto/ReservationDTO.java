5914c28f6fab2ff6e2352ebe755e174142ef5a08
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 10:50:05 +0400

    Update ReservationDTO.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/ReservationDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/ReservationDTO.java
@@ -1,4 +1,19 @@
 package com.cl.LibraryNetwork.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationDTO {
+    private Long id;
+    private LocalDate reservationDate;
+    private Long memberId;
+    private Long bookId;
 }
