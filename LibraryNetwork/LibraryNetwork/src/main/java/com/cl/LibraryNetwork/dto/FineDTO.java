5f8c84fc62752829d0b9f17a2d5545044fc09978
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 11:58:52 +0400

    fix datatype

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/FineDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/FineDTO.java
@@ -5,12 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class FineDTO {
     private Long id;
-    private Double amount;
+    private BigDecimal amount;
     private Long loanId;
 }
