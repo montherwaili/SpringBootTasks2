3e27951f61d188cf5538cb3dade1afa6c5f6265d
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 14:36:32 +0400

    Update EventController.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/EventController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/EventController.java
@@ -2,32 +2,44 @@ package com.cl.LibraryNetwork.Controllers;
 
 import com.cl.LibraryNetwork.Entities.Event;
 import com.cl.LibraryNetwork.Services.EventService;
+import com.cl.LibraryNetwork.dto.EventDTO;
+import com.cl.LibraryNetwork.mapper.EventMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
+@RequiredArgsConstructor
 public class EventController {
-    private final EventService service;
 
-    public EventController(EventService service) {
-        this.service = service;
+    private final EventService eventService;
+    private final EventMapper eventMapper;
+
+    @PostMapping
+    public EventDTO createEvent(@RequestBody Event event) {
+        return eventMapper.convertToDTO(eventService.save(event));
     }
 
     @GetMapping
-    public List<Event> getAll() { return service.getAll(); }
+    public List<EventDTO> getAllEvents() {
+        return eventMapper.convertToDTO(eventService.findAll());
+    }
 
     @GetMapping("/{id}")
-    public Event getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Event create(@RequestBody Event entity) { return service.save(entity); }
+    public EventDTO getEventById(@PathVariable Long id) {
+        return eventMapper.convertToDTO(eventService.findById(id));
+    }
 
     @PutMapping("/{id}")
-    public Event update(@PathVariable Long id, @RequestBody Event entity) {
-        return service.update(id, entity);
+    public EventDTO updateEvent(@PathVariable Long id, @RequestBody Event event) {
+        event.setId(id);
+        return eventMapper.convertToDTO(eventService.save(event));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void deleteEvent(@PathVariable Long id) {
+        eventService.deleteById(id);
+    }
 }
