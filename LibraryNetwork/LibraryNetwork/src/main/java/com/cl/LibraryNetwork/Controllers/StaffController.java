7f82c0ffbb52f4061a2e9933aef8afe02ae5f4c9
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 14:36:59 +0400

    Update StaffController.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/StaffController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/StaffController.java
@@ -1,33 +1,46 @@
 package com.cl.LibraryNetwork.Controllers;
 
+
 import com.cl.LibraryNetwork.Entities.Staff;
 import com.cl.LibraryNetwork.Services.StaffService;
+import com.cl.LibraryNetwork.dto.StaffDTO;
+import com.cl.LibraryNetwork.mapper.StaffMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/staff")
+@RequiredArgsConstructor
 public class StaffController {
-    private final StaffService service;
 
-    public StaffController(StaffService service) {
-        this.service = service;
+    private final StaffService staffService;
+    private final StaffMapper staffMapper;
+
+    @PostMapping
+    public StaffDTO createStaff(@RequestBody Staff staff) {
+        return staffMapper.convertToDTO(staffService.save(staff));
     }
 
     @GetMapping
-    public List<Staff> getAll() { return service.getAll(); }
+    public List<StaffDTO> getAllStaff() {
+        return staffMapper.convertToDTO(staffService.findAll());
+    }
 
     @GetMapping("/{id}")
-    public Staff getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Staff create(@RequestBody Staff entity) { return service.save(entity); }
+    public StaffDTO getStaffById(@PathVariable Long id) {
+        return staffMapper.convertToDTO(staffService.findById(id));
+    }
 
     @PutMapping("/{id}")
-    public Staff update(@PathVariable Long id, @RequestBody Staff entity) {
-        return service.update(id, entity);
+    public StaffDTO updateStaff(@PathVariable Long id, @RequestBody Staff staff) {
+        staff.setId(id);
+        return staffMapper.convertToDTO(staffService.save(staff));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void deleteStaff(@PathVariable Long id) {
+        staffService.deleteById(id);
+    }
 }
