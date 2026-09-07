36c08b33371a8a7139c0693ffaa9d77670125581
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 10:50:10 +0400

    Update StaffDTO.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/StaffDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/StaffDTO.java
@@ -1,4 +1,18 @@
 package com.cl.LibraryNetwork.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class StaffDTO {
+    private Long id;
+    private String name;
+    private String role;
+    private Long branchId;
 }
