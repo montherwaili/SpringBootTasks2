14358f1e12a7811ca644349fb42a9dd8e14ae9b2
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 10:50:07 +0400

    Update ReviewDTO.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/ReviewDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/ReviewDTO.java
@@ -1,4 +1,18 @@
 package com.cl.LibraryNetwork.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReviewDTO {
+    private Long id;
+    private String comment;
+    private Integer rating;
+    private Long memberId;
+    private Long bookId;
 }
