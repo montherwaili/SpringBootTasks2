2784150456d45b3be0ef702dc64f8f156e6ca739
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 10:50:01 +0400

    Update MemberDTO.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/MemberDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/MemberDTO.java
@@ -1,4 +1,16 @@
 package com.cl.LibraryNetwork.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MemberDTO {
+    private Long id;
+    private String name;
+    private String email;
 }
