19303dc06eaecd3a6f00255bc8784466a3e8dc66
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 11:59:42 +0400

    Update CategoryMapper.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/mapper/CategoryMapper.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/mapper/CategoryMapper.java
@@ -1,4 +1,26 @@
 package com.cl.LibraryNetwork.mapper;
 
+import com.cl.LibraryNetwork.dto.CategoryDTO;
+import com.cl.LibraryNetwork.Entities.Category;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class CategoryMapper {
+
+    public CategoryDTO convertToDTO(Category entity) {
+        if (entity == null) return null;
+        return CategoryDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .build();
+    }
+
+    public List<CategoryDTO> convertToDTO(List<Category> entities) {
+        return entities.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 }
