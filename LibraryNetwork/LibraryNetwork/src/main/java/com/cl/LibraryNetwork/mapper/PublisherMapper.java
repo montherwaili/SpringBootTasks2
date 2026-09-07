b6bcab22cf9b6a91780c2ecb5486eed7fa478c19
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 11:59:58 +0400

    Update PublisherMapper.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/mapper/PublisherMapper.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/mapper/PublisherMapper.java
@@ -1,4 +1,27 @@
 package com.cl.LibraryNetwork.mapper;
 
+
+import com.cl.LibraryNetwork.dto.PublisherDTO;
+import com.cl.LibraryNetwork.Entities.Publisher;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class PublisherMapper {
+
+    public PublisherDTO convertToDTO(Publisher entity) {
+        if (entity == null) return null;
+        return PublisherDTO.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .build();
+    }
+
+    public List<PublisherDTO> convertToDTO(List<Publisher> entities) {
+        return entities.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 }
