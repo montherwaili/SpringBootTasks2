67d2e783bc7be28f5ab71ae5b1f3fd9cd0e2ae10
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 14:36:46 +0400

    Update PublisherController.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/PublisherController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/PublisherController.java
@@ -1,33 +1,46 @@
 package com.cl.LibraryNetwork.Controllers;
 
+
 import com.cl.LibraryNetwork.Entities.Publisher;
 import com.cl.LibraryNetwork.Services.PublisherService;
+import com.cl.LibraryNetwork.dto.PublisherDTO;
+import com.cl.LibraryNetwork.mapper.PublisherMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/publishers")
+@RequiredArgsConstructor
 public class PublisherController {
-    private final PublisherService service;
 
-    public PublisherController(PublisherService service) {
-        this.service = service;
+    private final PublisherService publisherService;
+    private final PublisherMapper publisherMapper;
+
+    @PostMapping
+    public PublisherDTO createPublisher(@RequestBody Publisher publisher) {
+        return publisherMapper.convertToDTO(publisherService.save(publisher));
     }
 
     @GetMapping
-    public List<Publisher> getAll() { return service.getAll(); }
+    public List<PublisherDTO> getAllPublishers() {
+        return publisherMapper.convertToDTO(publisherService.findAll());
+    }
 
     @GetMapping("/{id}")
-    public Publisher getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Publisher create(@RequestBody Publisher entity) { return service.save(entity); }
+    public PublisherDTO getPublisherById(@PathVariable Long id) {
+        return publisherMapper.convertToDTO(publisherService.findById(id));
+    }
 
     @PutMapping("/{id}")
-    public Publisher update(@PathVariable Long id, @RequestBody Publisher entity) {
-        return service.update(id, entity);
+    public PublisherDTO updatePublisher(@PathVariable Long id, @RequestBody Publisher publisher) {
+        publisher.setId(id);
+        return publisherMapper.convertToDTO(publisherService.save(publisher));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void deletePublisher(@PathVariable Long id) {
+        publisherService.deleteById(id);
+    }
 }
