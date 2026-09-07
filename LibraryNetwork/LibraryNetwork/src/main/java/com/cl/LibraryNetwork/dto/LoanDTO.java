c85f5e96e545a704d619621543d8ee295a5ab41b
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 10:49:59 +0400

    Update LoanDTO.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/LoanDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/LoanDTO.java
@@ -1,4 +1,21 @@
 package com.cl.LibraryNetwork.dto;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoanDTO {
+    private Long id;
+    private LocalDate loanDate;
+    private LocalDate dueDate;
+    private Long memberId;
+    private Long bookId;
 }
