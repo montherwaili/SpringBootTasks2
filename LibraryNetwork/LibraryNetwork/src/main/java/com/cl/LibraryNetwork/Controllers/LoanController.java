67b0f49bc34fb50eef0d6a16d73dd91aa411bf21
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 14:36:41 +0400

    Update LoanController.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/LoanController.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/Controllers/LoanController.java
@@ -1,33 +1,46 @@
 package com.cl.LibraryNetwork.Controllers;
 
+
 import com.cl.LibraryNetwork.Entities.Loan;
 import com.cl.LibraryNetwork.Services.LoanService;
+import com.cl.LibraryNetwork.dto.LoanDTO;
+import com.cl.LibraryNetwork.mapper.LoanMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/loans")
+@RequiredArgsConstructor
 public class LoanController {
-    private final LoanService service;
 
-    public LoanController(LoanService service) {
-        this.service = service;
+    private final LoanService loanService;
+    private final LoanMapper loanMapper;
+
+    @PostMapping
+    public LoanDTO createLoan(@RequestBody Loan loan) {
+        return loanMapper.convertToDTO(loanService.save(loan));
     }
 
     @GetMapping
-    public List<Loan> getAll() { return service.getAll(); }
+    public List<LoanDTO> getAllLoans() {
+        return loanMapper.convertToDTO(loanService.findAll());
+    }
 
     @GetMapping("/{id}")
-    public Loan getById(@PathVariable Long id) { return service.getById(id); }
-
-    @PostMapping
-    public Loan create(@RequestBody Loan entity) { return service.save(entity); }
+    public LoanDTO getLoanById(@PathVariable Long id) {
+        return loanMapper.convertToDTO(loanService.findById(id));
+    }
 
     @PutMapping("/{id}")
-    public Loan update(@PathVariable Long id, @RequestBody Loan entity) {
-        return service.update(id, entity);
+    public LoanDTO updateLoan(@PathVariable Long id, @RequestBody Loan loan) {
+        loan.setId(id);
+        return loanMapper.convertToDTO(loanService.save(loan));
     }
 
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) { service.delete(id); }
+    public void deleteLoan(@PathVariable Long id) {
+        loanService.deleteById(id);
+    }
 }
