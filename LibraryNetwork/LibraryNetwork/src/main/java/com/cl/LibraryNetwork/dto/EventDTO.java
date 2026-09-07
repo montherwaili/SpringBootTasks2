00305a50a7095a674f32d3c65d718d81176a37d0
Almonther Alwaili <montherwaili@gmail.com>
2026-09-07 10:49:54 +0400

    Update EventDTO.java

--- LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/EventDTO.java
+++ LibraryNetwork/LibraryNetwork/src/main/java/com/cl/LibraryNetwork/dto/EventDTO.java
@@ -1,4 +1,19 @@
 package com.cl.LibraryNetwork.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventDTO {
+    private Long id;
+    private String title;
+    private LocalDate eventDate;
+    private Long branchId;
 }
